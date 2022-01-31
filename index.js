72460901ce07590249f9a11430f40eda20f78cf5
shu1007 <shuu.na1007@icloud.com>
2022-02-01 07:58:31 +0900

    loopするように

--- index.js
+++ index.js
@@ -7,33 +7,50 @@ const searchWord = (regexp) =>
     data.match(regexp)?.map((w) => w.substring(1, w.length - 1))
 
 const readlineSync = require('readline-sync')
-const greens = readlineSync.question('Enter the green letters(e.g. w....)> ')
-if (greens && !/^[a-z\.]{5}$/.test(greens)) {
-    console.error('Invalid string.')
-    exit(1)
-}
+const chars = { greens: '', yellows: '', blacks: '' }
 
-const yellows = readlineSync.question('Enter the yellow letters(e.g. abc)> ')
-if (yellows && !/^[a-z]{1,5}$/.test(yellows)) {
-    console.error('Invalid string.')
-    exit(1)
-}
-const blacks = readlineSync.question('Enter the black letters(e.g. abc)> ')
-if (blacks && !/^[a-z]+$/.test(blacks)) {
-    console.error('Invalid string.')
-    exit(1)
-}
+while (true) {
+    chars.greens = readlineSync.question(
+        `Enter the green letters(${
+            chars.greens != '' ? `previous: ${chars.greens}` : 'e.g. w....'
+        })> `
+    )
+    if (chars.greens && !/^[a-z\.]{5}$/.test(chars.greens)) {
+        console.error('Invalid string.')
+        exit(1)
+    }
 
-const greenPattern = !greens || greens == '.....' ? '' : `(?=${greens})`
-const yellowPatterns = yellows.split('').map((w) => `(?=.*${w})`)
-const blackPatterns = blacks.split('').map((w) => `(?!.*${w})`)
-const pattern = new RegExp(
-    `\\n${greenPattern}${yellowPatterns.join('')}${blackPatterns.join(
-        ''
-    )}.....\\n`,
-    'g'
-)
-console.log()
-const result = searchWord(pattern)
+    chars.yellows = readlineSync.question(
+        `Enter the yellow letters(${
+            chars.yellows != '' ? `previous: ${chars.yellows}` : 'e.g. abc'
+        })> `
+    )
+    if (chars.yellows && !/^[a-z]{1,5}$/.test(chars.yellows)) {
+        console.error('Invalid string.')
+        exit(1)
+    }
+    chars.blacks = readlineSync.question(
+        `Enter the black letters(${
+            chars.blacks != '' ? `previous: ${chars.blacks}` : 'e.g. abc'
+        })> `
+    )
+    if (chars.blacks && !/^[a-z]+$/.test(chars.blacks)) {
+        console.error('Invalid string.')
+        exit(1)
+    }
 
-result ? result.forEach((w) => console.log(w)) : console.log('No result.')
+    const greenPattern =
+        !chars.greens || chars.greens == '.....' ? '' : `(?=${chars.greens})`
+    const yellowPatterns = chars.yellows.split('').map((w) => `(?=.*${w})`)
+    const blackPatterns = chars.blacks.split('').map((w) => `(?!.*${w})`)
+    const pattern = new RegExp(
+        `\\n${greenPattern}${yellowPatterns.join('')}${blackPatterns.join(
+            ''
+        )}.....\\n`,
+        'g'
+    )
+    console.log()
+    const result = searchWord(pattern)
+
+    result ? result.forEach((w) => console.log(w)) : console.log('No result.')
+}
