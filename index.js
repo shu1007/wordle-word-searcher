42e68a5743981225c670f43df9081b9e3bba3276
shu1007 <shuu.na1007@icloud.com>
2022-02-01 07:16:55 +0900

    メッセージ修正

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const searchWord = (regexp) =>
     data.match(regexp)?.map((w) => w.substring(1, w.length - 1))
 
 const readlineSync = require('readline-sync')
-const greens = readlineSync.question('put green chracters(e.g. w....)> ')
+const greens = readlineSync.question('Enter the green letters(e.g. w....)> ')
 if (greens && !/^[a-z\.]{5}$/.test(greens)) {
-    console.error('invalid str.')
+    console.error('Invalid string.')
     exit(1)
 }
 
-const yellows = readlineSync.question('put yellow chracters(e.g. abc)> ')
+const yellows = readlineSync.question('Enter the yellow letters(e.g. abc)> ')
 if (yellows && !/^[a-z]{1,5}$/.test(yellows)) {
-    console.error('invalid str.')
+    console.error('Invalid string.')
     exit(1)
 }
 
@@ -28,4 +28,4 @@ const pattern = new RegExp(
 console.log()
 const result = searchWord(pattern)
 
-result ? result.forEach((w) => console.log(w)) : console.log('no result.')
+result ? result.forEach((w) => console.log(w)) : console.log('No result.')
